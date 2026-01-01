917391d150ee0b9ea329ce3cad91d5790b579635
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-01-02 00:31:09 +0200

    Add Day 01 proplems in NTI

---
+++ Day 01/02.dart
@@ -0,0 +1,11 @@
+void main() {
+  int age = 23;
+  double salary = 5000.75;
+  String name = "Salah";
+  bool isStudent = true;
+
+  print(age);
+  print(salary);
+  print(name);
+  print(isStudent);
+}
