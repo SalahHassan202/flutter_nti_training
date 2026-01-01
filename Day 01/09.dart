620e5fabd2e10d924f658b4ab8a1fd3323674d95
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-01-02 00:32:58 +0200

    Add Day 01 proplems in NTI

---
+++ Day 01/09.dart
@@ -0,0 +1,20 @@
+void main()
+{
+  int age = 23;
+  double height = 175.5;
+  String name = "Salah";
+  bool isDeveloper = true;
+
+  var city = "Cairo";
+
+  final String university = "Engineering";
+  const int year = 2025;
+
+  print("Name: $name");
+  print("Age: $age");
+  print("Height: $height");
+  print("Developer: $isDeveloper");
+  print("City: $city");
+  print("University: $university");
+  print("Year: $year");
+}
