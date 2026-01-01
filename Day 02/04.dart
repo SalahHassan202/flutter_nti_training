e8942620ed58c6f8e077161a988a558691ed50d2
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-01-02 00:39:26 +0200

    Added Day 02 proplems in NTI

---
+++ Day 02/04.dart
@@ -0,0 +1,11 @@
+void main() {
+  List<int> nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  for (var x in nums) {
+    if (x % 2 == 0) {
+      print("$x is even");
+    } else {
+      print("$x is odd");
+    }
+  }
+}
