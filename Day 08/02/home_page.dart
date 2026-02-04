1d22b4cc8de1766623f70ad4f6db1ee8fb312f08
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-02-04 11:51:09 +0200

    Added Day 08 proplems in NTI

---
+++ Day 08/02/home_page.dart
@@ -0,0 +1,58 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Dio dio = Dio();
+
+  String image = "";
+
+  Future<void> getData() async {
+    var response = await dio.get("https://dog.ceo/api/breeds/image/random");
+    var jsonBody = response.data;
+
+    image = jsonBody["message"];
+
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Api NTI"), centerTitle: true),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (image.isNotEmpty)
+              Image.network(image, height: 300, fit: BoxFit.fill)
+            else
+              const CircularProgressIndicator(),
+
+            const SizedBox(height: 20),
+
+            IconButton(
+              onPressed: () {
+                getData();
+              },
+              icon: const Icon(Icons.refresh, size: 30),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
