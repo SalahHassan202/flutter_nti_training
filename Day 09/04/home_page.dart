9c127b806644381312ec82eab08bc2ff530cc671
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-02-07 12:34:22 +0200

    Added Day 09 proplems in NTI

---
+++ Day 09/04/home_page.dart
@@ -0,0 +1,57 @@
+import 'package:api_app/list_view_widget.dart';
+import 'package:api_app/model.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Dio dio = Dio();
+  List<Model> data = [];
+  Future<List<Model>> getData() async {
+    var response = await dio.get(
+      "https://dummyjson.com/products/category/mens-shirts",
+    );
+
+    List jsonBody = response.data['products'];
+    return jsonBody.map((e) => Model.fromJson(e)).toList();
+  }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   getData();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffF5F5F5),
+      appBar: AppBar(
+        title: Text("API COURSE"),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: FutureBuilder<List<Model>>(
+        future: getData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text("Error"));
+          } else if (snapshot.hasData) {
+            return ListViewWidget(data: snapshot.data!);
+          }
+
+          return Container();
+        },
+      ),
+    );
+  }
+}
+
