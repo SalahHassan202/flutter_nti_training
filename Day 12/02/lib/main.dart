e6d7f5e8e9fd2e05cf18cf091921d9d4f62efd5f
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-02-14 14:54:42 +0200

    Added Day 12 proplems in NTI

---
+++ Day 12/02/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'features/home/views/home_page.dart';
+
+void main() {
+  Bloc.observer = MyBlocObserver();
+  runApp(const MyApp());
+}
+
+class MyBlocObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
