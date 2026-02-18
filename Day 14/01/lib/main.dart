162e1104fb205f13e0809bde72abc0dd14171fda
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-02-18 13:49:28 +0200

    Added Day 14 proplems in NTI

---
+++ Day 14/01/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:app123/cubit/cubit/auth_cubit.dart';
+import 'package:app123/view/screens/Login/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => AuthCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: LoginScreen(),
+      ),
+    );
+  }
+}
